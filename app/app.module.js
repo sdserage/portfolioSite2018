5e292efc688dc11287db7f6604c4551c71c36fff
Scott Serage <xttocsurge51@gmail.com>
2018-02-14 20:58:28 -0700

    Combined landing into about

--- app/app.module.js
+++ app/app.module.js
@@ -2,7 +2,6 @@
 
 angular.module("portfolioApp", [
   'menu',
-  'landing',
   'about',
   'skills',
   'projects',
--- app/landing/landing.component.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('landing').component('landing', {
-  templateUrl: 'app/landing/landing.template.html',
-  controller: ($scope)=>{
-    console.log('landing');
-  }
-});
