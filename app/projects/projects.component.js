78af8d0d7c54ef2bbcaf8a1c397c714c46f97c27
Scott Serage <xttocsurge51@gmail.com>
2018-02-19 16:17:35 -0700

    Basically aquired mvp

--- app/projects/projects.component.js
+++ app/projects/projects.component.js
@@ -16,12 +16,19 @@ const trippinPaths = [
 const amsContent = "Air Management Specialists' website is a personal project using React that demonstrates an easy to use wizard and a robust filter toolbar.";
 const trippinContent = "Trippin' is an online travel planner that uses several Material-Ui components as well as Yelp's api. It is a group project that uses React.";
 
+const amsGitHub = "https://github.com/sdserage/ams";
+const amsWebsite = "http://ams.sdserage.com/";
+const trippinGitHub = "https://github.com/Vacation-Application/vacation";
+const trippinWebsite = "http://trippinonline.com/#/";
+
 angular.module('projects').component('projects', {
   templateUrl: 'app/projects/projects.template.html',
   controller: ($scope)=>{
     $scope.pathIndex = 0;
     $scope.project = amsPaths;
     $scope.projectContent = amsContent;
+    $scope.website = amsWebsite;
+    $scope.gitHub = amsGitHub;
 
     $scope.incrementIndex = ()=>{
       $scope.pathIndex ++;
@@ -42,14 +49,25 @@ angular.module('projects').component('projects', {
         case 'ams':
           $scope.project = amsPaths;
           $scope.projectContent = amsContent;
+          $scope.website = amsWebsite;
+          $scope.gitHub = amsGitHub;
           break;
         case 'trippin':
           $scope.project = trippinPaths;
           $scope.projectContent = trippinContent;
+          $scope.website = trippinWebsite;
+          $scope.gitHub = trippinGitHub;
           break;
         default:
         $scope.project = amsPaths;
+        $scope.projectContent = amsContent;
+        $scope.website = amsWebsite;
+        $scope.gitHub = amsGitHub;
       }
+    }
+
+    $scope.linkOutside = url =>{
+      $window.open(url);
     }
   }
 });
